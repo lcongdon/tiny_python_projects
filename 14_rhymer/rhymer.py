ec2e50de7b9208728118c663217919dc0fbdbdc8
Lee Congdon <lee@lcongdon.com>
2021-08-16 15:32:30 -0400

    Stemmer implementation

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -6,14 +6,57 @@ Purpose: Tiny Python Projects rhymer exercise
 """
 
 import argparse
-from re import S
+import re
+import string as s
+
+VOWELS = "aeiou"
+CONSONANTS = "".join([c for c in s.ascii_lowercase if c not in VOWELS])
+PREFIXES = [
+    "bl",
+    "br",
+    "ch",
+    "cl",
+    "cr",
+    "dr",
+    "fl",
+    "fr",
+    "gl",
+    "gr",
+    "pl",
+    "pr",
+    "sc",
+    "sh",
+    "sk",
+    "sl",
+    "sm",
+    "sn",
+    "sp",
+    "st",
+    "sw",
+    "th",
+    "tr",
+    "tw",
+    "thw",
+    "wh",
+    "wr",
+    "sch",
+    "scr",
+    "shr",
+    "sph",
+    "spl",
+    "spr",
+    "squ",
+    "str",
+    "thr",
+]
 
 
 def get_args():
     """Parse arguments"""
 
     parser = argparse.ArgumentParser(
-        description="Make rhyming 'words'", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Make rhyming 'words'",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     # parser.add_argument('-a',
@@ -46,64 +89,35 @@ def get_args():
 
     return parser.parse_args()
 
+
 def stemmer(word):
     """Return leading consonants (if any), and 'stem' of word"""
-    pass
+    pattern = f"([{CONSONANTS}]+)?([{VOWELS}])(.*)"
+    word = word.lower()
+    match = re.match(pattern, word, re.IGNORECASE)
+    if match:
+        part1 = match.group(1) or ""
+        part2 = match.group(2) or ""
+        part3 = match.group(3) or ""
+        return (part1, part2 + part3)
+    else:
+        return (word, "")
+
 
 def test_stemmer():
     """Test stemmer"""
-    assert stemmer('') == ('', '')
-    assert stemmer('cake') == ('c', 'ake')
-    assert stemmer('chair') == ('ch', 'air')
-    assert stemmer('APPLE') == ('', 'apple')
-    assert stemmer('RDZNL') == ('rdznl', '')
-    assert stemmer('123') == ('123', '')
+    assert stemmer("") == ("", "")
+    assert stemmer("cake") == ("c", "ake")
+    assert stemmer("chair") == ("ch", "air")
+    assert stemmer("APPLE") == ("", "apple")
+    assert stemmer("RDZNL") == ("rdznl", "")
+    assert stemmer("123") == ("123", "")
+
 
 def main():
 
-    prefixes = [
-        "bl",
-        "br",
-        "ch",
-        "cl",
-        "cr",
-        "dr",
-        "fl",
-        "fr",
-        "gl",
-        "gr",
-        "pl",
-        "pr",
-        "sc",
-        "sh",
-        "sk",
-        "sl",
-        "sm",
-        "sn",
-        "sp",
-        "st",
-        "sw",
-        "th",
-        "tr",
-        "tw",
-        "thw",
-        "wh",
-        "wr",
-        "sch",
-        "scr",
-        "shr",
-        "sph",
-        "spl",
-        "spr",
-        "squ",
-        "str",
-        "thr",
-    ]
-    consonants = [
-
-    ]
     """Main program"""
-
+    print(test_stemmer(''))
     args = get_args()
     pos_arg = args.word
 
